3cc9ac0b482db112ec80f24cc1107e8bdd19f26b
rijuld <errijuldahiya@gmail.com>
2021-01-17 23:32:07 +0530

    added google auth to the login page

--- auctions/urls.py
+++ auctions/urls.py
@@ -1,11 +1,9 @@
-from django.urls import path,include
-from django.contrib import admin
-from django.views.generic import TemplateView
+from django.urls import path
 
 from . import views
 
 urlpatterns = [
-    path("index", views.index, name="index"),
+    path("", views.index, name="index"),
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
     path("register", views.register, name="register"),
